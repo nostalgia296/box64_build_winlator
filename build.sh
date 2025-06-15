5859bd8c9c430f71bf836def294c77c0a2f5b87c
nostalgia296 <3570965752@qq.com>
2025-06-15 15:02:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
 git clone https://github.com/ptitSeb/box64
 cd box64
-mkdir build
-cd build
-cmake .. -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DARM64=1 -DWINLATOR_GLIBC=1 -DARM_DYNAREC=1 -DBAD_SIGNAL=1 -DCMAKE_BUILD_TYPE=Release -DBOX32=1
-make -j
-mkdir -p ./usr/local/bin
-mv ./box64 ./usr/local/bin/
-patchelf --set-interpreter /data/data/com.winlator/files/rootfs/lib/ld-linux-aarch64.so.1 ./usr/local/bin/box64
-tar -cf ./box64_w10_box32.tar ./usr/local/bin/box64
-zstd -z ./box64_w10_box32.tar
-mv ./box64_w10_box32.tar.zst ./box64_w10_box32.whp
-cd ..
 mkdir buildNo32
 cd buildNo32
 cmake .. -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DARM64=1 -DWINLATOR_GLIBC=1 -DARM_DYNAREC=1 -DBAD_SIGNAL=1 -DCMAKE_BUILD_TYPE=Release
@@ -19,5 +8,5 @@ mkdir -p ./usr/local/bin
 mv ./box64 ./usr/local/bin/
 patchelf --set-interpreter /data/data/com.winlator/files/rootfs/lib/ld-linux-aarch64.so.1 ./usr/local/bin/box64
 tar -cf ./box64_w10.tar ./usr/local/bin/box64
-zstd -z ./box64_w10.tar
+zstd -19 ./box64_w10.tar --rm
 mv ./box64_w10.tar.zst ./box64_w10.whp
